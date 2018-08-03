de07ad6af28541ba9a4cb7baee9d343e4de37cb3
David Antliff <david.antliff@aviatnet.com>
2018-08-03 17:56:08 +1200

    Display subtrees on specified workspaces.

--- py3tree.py
+++ py3tree.py
@@ -5,41 +5,89 @@ import i3ipc
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--compat", action="store_true", help="Output compatible with i3tree")
+    parser.add_argument("-w", "--workspace", type=str, nargs="*", help="Workspace to display")
+    args = parser.parse_args()
+
     i3 = i3ipc.Connection()
     tree = i3.get_tree()
 
-    walk_tree(tree, 0)
+    if args.compat:
+        i3tree_walk_tree(tree, 0)
+    else:
+        walk_tree(tree, 0, filter_workspaces=args.workspace,
+                  mute=args.workspace is not None)
 
 
-def display_node(description, depth):
+def i3tree_display_node(description, depth):
     margin = "\t" * depth
     print("{margin}{description}".format(margin=margin, description=description))
     return depth + 1
 
 
-def walk_tree(node, depth):
+def i3tree_walk_tree(node, depth):
     type = node.type
     orientation = node.orientation
     layout = node.layout
     name = node.name
 
     if type == "workspace":
-        depth = display_node("Workspace {name} ({layout} - {orientation})"
-                             .format(name=name,
+        depth = i3tree_display_node("Workspace {name} ({layout} - {orientation})"
+                                    .format(name=name,
                                      layout=layout,
                                      orientation=orientation),
-                             depth)
+                                    depth)
 
     if type == "con":
         if orientation == "none":
             if name == "content" or not name.startswith("i3bar"):
-                depth = display_node("Window {name}".format(name=name), depth)
+                depth = i3tree_display_node("Window {name}".format(name=name), depth)
         else:
-            depth = display_node("Split ({layout} - {orientation})".format(layout=layout,
-                                                                          orientation=orientation), depth)
+            depth = i3tree_display_node("Split ({layout} - {orientation})".format(layout=layout,
+                                                                                  orientation=orientation), depth)
 
     for node in node.nodes:
-        walk_tree(node, depth)
+        i3tree_walk_tree(node, depth)
+
+
+def display_node(description, depth):
+    margin = "    " * depth
+    print("{margin}{description}".format(margin=margin, description=description))
+    return depth + 1
+
+
+def walk_tree(node, depth, filter_workspaces=None, mute=False):
+    type_ = node.type
+    orientation = node.orientation
+    layout = node.layout
+    name = node.name
+
+    if filter_workspaces is not None:
+        if type_ == "workspace":
+            if name not in filter_workspaces:
+                mute = True
+            else:
+                mute = False
+
+    if not mute:
+        if type_ == "workspace":
+            depth = display_node("Workspace {name} ({layout} - {orientation})"
+                                 .format(name=name,
+                                         layout=layout,
+                                         orientation=orientation),
+                                 depth)
+
+        if type_ == "con":
+            if orientation == "none":
+                if name == "content" or not name.startswith("i3bar"):
+                    depth = display_node("Window {name}".format(name=name), depth)
+            else:
+                depth = display_node("Split ({layout} - {orientation})".format(layout=layout,
+                                                                                      orientation=orientation), depth)
+
+    for node in node.nodes:
+        walk_tree(node, depth, filter_workspaces=filter_workspaces, mute=mute)
 
 
 if __name__ == '__main__':
