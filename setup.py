efc41b296fe680f5e6b066ac53306febcdf7086d
David Antliff <david.antliff@aviatnet.com>
2018-08-06 12:26:56 +1200

    Include license file in sdist. Add keywords and supported Python versions.

--- setup.py
+++ setup.py
@@ -5,26 +5,29 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="py3tree",
-    version="0.0.1",
+    version="0.0.2",
     author="David Antliff",
     author_email="david.antliff@gmail.com",
     description="Display the i3 tree for the current or all workspaces",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    keywords="i3 i3ipc",
     url="https://github.com/DavidAntliff/py3tree.git",
-    packages=setuptools.find_packages(),
+    packages=["py3tree"],
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4",
     install_requires=[
         "future>=0.16.0",
         "i3ipc>=1.5.1",
     ],
     entry_points={
-        'console_scripts': [
-            'py3tree = py3tree.py3tree:main'
+        "console_scripts": [
+            "py3tree = py3tree.py3tree:main"
         ]
     },
     classifiers=(
+        "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Environment :: X11 Applications",
         "Topic :: Desktop Environment :: Window Managers",
